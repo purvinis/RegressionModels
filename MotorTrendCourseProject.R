ab5504fb9522dcaf9dbfb8e1ef3e64f7dd4f7043
Purvinis <g.purvinis@gmail.com>
2021-05-28 06:29:43 -1000

    added plots and more trials

--- MotorTrendCourseProject.R
+++ MotorTrendCourseProject.R
@@ -1,6 +1,9 @@
 require(datasets)
 data(mtcars)
 library(ggplot2)
+require(ggplot2)
+install.packages("GGally")
+require(GGally)
 g1head(mtcars)
 #“Is an automatic or manual transmission better for MPG”
 # confounders: 
@@ -8,37 +11,46 @@ g1head(mtcars)
 fitall <- lm(mpg ~ ., data=mtcars)
 summary(fitall)
 
-# from p-data, predictors are correlated
+g1 <- ggpairs(mtcars,lower = list(continuous = wrap("points")),ggplot2::aes(colour=factor(am)))
+g1
+g2 <- ggcorr(mtcars, method = c("everything","spearman"))
+g2
+# Scatterplots of each pair of numeric variable are drawn on the left part 
+#of the figure. Pearson correlation is displayed on the right. 
+#Variable distribution is available on the diagonal.
 
 fit1 <- lm(mpg ~ am , data=mtcars)
 summary(fit1)  # p-value 0.00285. Reject
 e1 <- resid(fit1)
 
-fit2 <- lm(mpg ~ cyl,data=mtcars)
+fit2 <- lm(mpg ~ wt + cyl + disp +vs ,data=mtcars)
 summary(fit2)  # good model, 
 #e2 <- resid(fit2)
 
-fit3 <- lm(mpg ~ cyl + am,data=mtcars)
+fit3 <- lm(mpg[am==1] ~ cyl[am ==1] + wt[am ==1],data=mtcars)
 summary(fit3)  # bad am p-value 
 #value of the relationship between mpg and cyl remains
 #largely unchanged when considering am
 #check the residual variation. Should be almost no resid variability
 #after accounting for am
 
-fit4 <- lm(mpg ~ cyl + wt,data=mtcars)
+fit4 <- lm(mpg ~ am + cyl ,data=mtcars)  #cyl better than wt
 summary(fit4)  # wt and cyl stronger, am bad
 
-
-fit5 <- lm(mpg ~ cyl  +wt,data=mtcars)
+ncyl = 8
+fit5 <- lm(mpg[cyl==ncyl] ~ am[cyl ==ncyl] + cyl[cyl == ncyl] +wt[cyl==ncyl] +disp[cyl==ncyl],data=mtcars)
 summary(fit5)  # 
 
-anova(fit1,fit2, fit3, fit4, fit5)
-library(car)
-vif(fit3) #check for variance inflation
+fit6 <- lm(mpg ~ am +cyl + wt +disp + wt*disp,data=mtcars)
+summary(fit6)
+
+anova(fit2,fit4, fit6)
+#library(car)
+#vif(fit3) #check for variance inflation
 
 #havalues shows no outliers/influencersc
 #Modeling multivariate relationships is difficult.
 #Play around with simulations to see how the inclusion or exclustion of another variable can change
 #analyses
 
-plot(predict(fit3),resid(fit3 ))
+plot(predict(fit4),resid(fit4))
